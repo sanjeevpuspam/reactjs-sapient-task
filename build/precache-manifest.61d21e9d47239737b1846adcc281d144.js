5881d58adc2fc23623f9f71302a2dc81828b458e
jyoti@techmine.co.il <tftus@1989>
2020-12-21 21:50:04 +0530

    code to deployee

---
+++ build/precache-manifest.61d21e9d47239737b1846adcc281d144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc613ab1e75927d6c31875efc188986",
+    "url": "/reactjs-sapient-task/index.html"
+  },
+  {
+    "revision": "daf800dd6edab8902301",
+    "url": "/reactjs-sapient-task/static/css/main.712044df.chunk.css"
+  },
+  {
+    "revision": "81203598ab1e53e171ba",
+    "url": "/reactjs-sapient-task/static/js/2.c2b4036a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-sapient-task/static/js/2.c2b4036a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf800dd6edab8902301",
+    "url": "/reactjs-sapient-task/static/js/main.7733a263.chunk.js"
+  },
+  {
+    "revision": "a9aa2de2e1fd77809e31",
+    "url": "/reactjs-sapient-task/static/js/runtime-main.9d5b5324.js"
+  }
+]);
